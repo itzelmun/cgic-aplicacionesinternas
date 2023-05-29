aa37723c36c963625da2f689f9ca27b4e31e7ae0
itzelmun <munguiaitzel532@gmail.com>
2023-05-29 16:15:34 -0600

    jh

--- db/conexion.php
+++ db/conexion.php
@@ -4,4 +4,7 @@ $nombre_host = $_ENV['DB_HOST'];
 $usuario_bd = $_ENV['DB_USER'];
 $contrasena_bd = $_ENV['DB_PASS'];
 $base_datos_nombre = $_ENV['DB_NAME'];
+
+$maxUploadSize = $_ENV['UPLOAD_MAX_FILESIZE'];
+echo "El tamaño máximo de carga permitido es: " . $maxUploadSize;
 ?>
