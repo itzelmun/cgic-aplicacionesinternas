7e15fc34f05cd459deedd9fe0c4b9df2dcec92e9
itzelmun <munguiaitzel532@gmail.com>
2023-06-07 11:12:43 -0600

    cambios en conexion db

--- sourcecode/conexion.php
+++ sourcecode/conexion.php
@@ -5,6 +5,12 @@ $usuario_bd = $_ENV['DB_USER'];
 $contrasena_bd = $_ENV['DB_PASS'];
 $base_datos_nombre = $_ENV['DB_NAME'];
 
-$maxUploadSize = $_ENV['UPLOAD_MAX_FILESIZE'];
-echo "El tamaño máximo de carga permitido es: " . $maxUploadSize;
+//$maxUploadSize = $_ENV['UPLOAD_MAX_FILESIZE'];
+//echo "El tamaño máximo de carga permitido es: " . $maxUploadSize;
+
+$conexion = mysqli_connect($nombre_host, $usuario_bd, $contrasena_bd) or die(mysql_error()."<br/><br/><br/><ce$
+
+mysql_select_db($base_datos_nombre, $conexion) or die(mysql_error()."<br/><br/><br/><center>ERROR: Ha ocurrid$
+
+
 ?>
