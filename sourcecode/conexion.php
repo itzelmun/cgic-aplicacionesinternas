c9fd3691882615cb25fbafd6392d9845745c919f
itzelmun <munguiaitzel532@gmail.com>
2023-06-09 12:06:05 -0600

    se ´cambio tag mysql y php

--- sourcecode/conexion.php
+++ sourcecode/conexion.php
@@ -8,9 +8,9 @@ $base_datos_nombre = $_ENV['MYSQL_DATABASE'];
 //$maxUploadSize = $_ENV['UPLOAD_MAX_FILESIZE'];
 //echo "El tamaño máximo de carga permitido es: " . $maxUploadSize;
 
-$conexion = mysqli_connect($nombre_host, $usuario_bd, $contrasena_bd) or die(mysql_error()."<br/><br/><br/><ce$
+$conexion = mysqli_connect($nombre_host, $usuario_bd, $contrasena_bd) or die(mysqli_error($conexion)."<br/><br/><br/><center>");
+mysqli_select_db($conexion, $base_datos_nombre) or die(mysqli_error($conexion)."<br/><br/><br/><center>ERROR: Ha ocurrido un problema al seleccionar la base de datos.</center>");
 
-mysql_select_db($base_datos_nombre, $conexion) or die(mysql_error()."<br/><br/><br/><center>ERROR: Ha ocurrid$
 
 
 ?>
