0be9ed915e277bb8cfe784a715b0906572b28a8a
itzelmun <munguiaitzel532@gmail.com>
2023-06-21 13:44:53 -0600

    se cambio variables

--- db/phpmyadmin/conexion.php
+++ db/phpmyadmin/conexion.php
@@ -2,7 +2,7 @@
 
 $nombre_host = $_ENV['DB_HOST'];
 $usuario_bd = $_ENV['MYSQL_USER'];
-$contrasena_bd = $_ENV['MYSQL_ROOT_PASSWORD'];
+$contrasena_bd = $_ENV['MYSQL_PASSWORD'];
 $base_datos_nombre = $_ENV['MYSQL_DATABASE'];
 
 //$maxUploadSize = $_ENV['UPLOAD_MAX_FILESIZE'];
