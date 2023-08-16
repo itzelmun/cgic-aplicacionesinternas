12c3aa16cce2e0c655ef2263bfec10bb1127b864
itzelmun <munguiaitzel532@gmail.com>
2023-08-16 17:27:33 -0600

    cambio de conexion

--- sourcecode/sistemaMensajes/conexion.php
+++ sourcecode/sistemaMensajes/conexion.php
@@ -1,9 +1,14 @@
 <?php
 	//definir datos de acceso (servidor, usuario, contraseña, bd)
-	$servidor = "www.ucol.mx";
-	$usuario = "wcgic";
-	$contrasena = "4TcCF";
-	$bd = "cgic";
+	//$servidor = "www.ucol.mx";
+	//$usuario = "wcgic";
+	//$contrasena = "4TcCF";
+	//$bd = "cgic";
+	$servidor = $_ENV['DB_HOST'];
+	$usuario = $_ENV['MYSQL_USER'];
+	$contrasena = $_ENV['MYSQL_PASSWORD'];
+	$bd = $_ENV['MYSQL_DATABASE'];
+
 	
 	//crear la conexión
 	$cnx = mysqli_connect($servidor, $usuario, $contrasena, $bd);
