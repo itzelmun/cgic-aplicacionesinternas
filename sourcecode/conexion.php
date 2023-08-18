02ade2989a1c6d0056c0fde5c947a171288f1afd
itzelmun <munguiaitzel532@gmail.com>
2023-08-18 15:42:21 -0600

    cambios en codigo fuente

--- sourcecode/conexion.php
+++ sourcecode/conexion.php
@@ -5,7 +5,13 @@ $usuario_bd = $_ENV['MYSQL_USER'];
 $contrasena_bd = $_ENV['MYSQL_PASSWORD'];
 $base_datos_nombre = $_ENV['MYSQL_DATABASE'];
 
-$conexion = mysql_connect($nombre_host, $usuario_bd, $contrasena_bd,$base_datos_nombre) or die(mysql_error($conexion)."<br/><br/><br/><center>");
-mysql_select_db($conexion, $base_datos_nombre) or die(mysql_error($conexion)."<br/><br/><br/><center>ERROR: Ha ocurrido un problema al seleccionar la base de datos.</center>");
+$conexion = new mysqli($nombre_host, $usuario_bd, $contrasena_bd, $base_datos_nombre);
 
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+
+if (!$conexion->select_db($base_datos_nombre)) {
+    die("Error al seleccionar la base de datos: " . $conexion->error);
+}
 ?>
