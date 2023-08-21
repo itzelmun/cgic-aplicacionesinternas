bb450429693ae82187ceb95b221ab55284c9d861
itzelmun <munguiaitzel532@gmail.com>
2023-08-21 13:32:10 -0600

    cambio en variables2

--- sourcecode/conexion.php
+++ sourcecode/conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$nombre_host = getenv['MYSQL_HOST'];
-$usuario_bd = getenv['MYSQL_USER'];
-$contrasena_bd =getenv['MYSQL_PASSWORD'];
-$base_datos_nombre = getenv['MYSQL_DATABASE'];
+$nombre_host = getenv('MYSQL_HOST');
+$usuario_bd = getenv('MYSQL_USER');
+$contrasena_bd =getenv('MYSQL_PASSWORD');
+$base_datos_nombre = getenv('MYSQL_DATABASE');
 
 
 $conexion = mysqli_connect($nombre_host, $usuario_bd, $contrasena_bd, $base_datos_nombre);
