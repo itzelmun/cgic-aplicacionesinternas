a5545b77dbbf3601068974c13e242177d6cac2c6
itzelmun <munguiaitzel532@gmail.com>
2023-08-21 13:17:28 -0600

    dgfgh

--- sourcecode/conexion.php
+++ sourcecode/conexion.php
@@ -1,13 +1,12 @@
 <?php
 
-$nombre_host = $_ENV['DB_HOST'];
-$usuario_bd = $_ENV['MYSQL_USER'];
-$contrasena_bd = $_ENV['MYSQL_PASSWORD'];
-$base_datos_nombre = $_ENV['MYSQL_DATABASE'];
-$puerto = $_ENV['DB_PORT']; // Asegúrate de definir DB_PORT en tus variables de entorno
+$nombre_host = getenv['MYSQL_HOST'];
+$usuario_bd = getenv['MYSQL_USER'];
+$contrasena_bd =getenv['MYSQL_PASSWORD'];
+$base_datos_nombre = getenv['MYSQL_DATABASE'];
 
 
-$conexion = mysqli_connect($nombre_host, $usuario_bd, $contrasena_bd);
+$conexion = mysqli_connect($nombre_host, $usuario_bd, $contrasena_bd, $base_datos_nombre);
 
 if (!$conexion) {
     die("Error de conexión: Ha ocurrido un error al CONECTAR la base de datos" . mysqli_error());
