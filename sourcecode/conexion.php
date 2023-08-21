5797e5fe48196c831c198fb5c9a86be910f30093
itzelmun <munguiaitzel532@gmail.com>
2023-08-21 12:04:04 -0600

    cdasd

--- sourcecode/conexion.php
+++ sourcecode/conexion.php
@@ -1,8 +1,8 @@
 <?php
 
-$nombre_host = $_ENV['DB_HOST'];
+$nombre_host = $_ENV['MYSQL_HOST'];
 $usuario_bd = $_ENV['MYSQL_USER'];
-$contrasena_bd = $_ENV['MYSQL_PASSWORD'];
+$contrasena_bd = $_ENV['MYSQL_ROOT_PASSWORD'];
 $base_datos_nombre = $_ENV['MYSQL_DATABASE'];
 $puerto = $_ENV['DB_PORT']; // Asegúrate de definir DB_PORT en tus variables de entorno
 
