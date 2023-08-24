c22f068fbc07ee800e50a2a939f48fad12c89c34
itzelmun <munguiaitzel532@gmail.com>
2023-08-24 15:37:19 -0600

    gh

--- sourcecode/sni_nivel.php
+++ sourcecode/sni_nivel.php
@@ -10,12 +10,6 @@ tr:nth-child(even){ background: #FFFFFF;}
 header('Content-Type: text/html; charset=UTF-8');
 ?>
 </style>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-</head>
-<body>
     <h2 class="verde"><img src="imagenes/vineta.jpg" /><strong>SNI Nivel</strong></h2>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <div align="right"><p align="right"><strong><font color="#FD8431">Buscar Nivel</font></strong>
