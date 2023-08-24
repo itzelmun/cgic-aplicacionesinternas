feeb3a52cbd845043332462a901614334d9ca7aa
itzelmun <munguiaitzel532@gmail.com>
2023-08-24 09:48:27 -0600

    cambio de variables

--- sourcecode/conexion.php
+++ sourcecode/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$nombre_host = '127.0.0.1';
-$usuario_bd = 'wcgic';
-$contrasena_bd ='4TcCF';
-$base_datos_nombre = 'cgic';
+$nombre_host =getenv('MYSQL_HOST');
+$usuario_bd = getenv('MYSQL_USER');
+$contrasena_bd =getenv('MYSQL_PASSWORD');
+$base_datos_nombre = getenv('MYSQL_DATABASE');
 
 $conexion = mysqli_connect($nombre_host, $usuario_bd, $contrasena_bd, $base_datos_nombre);
 
