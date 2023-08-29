cdc6b513c90535c38e051b6169b8a3c4c163c389
itzelmun <munguiaitzel532@gmail.com>
2023-08-29 12:14:51 -0600

    funciona

--- sourcecode/sni_nivel.php
+++ sourcecode/sni_nivel.php
@@ -47,10 +47,8 @@ tr:nth-child(even){ background: #FFFFFF;}
                 <strong>Vigencia</strong>
             </td>
         </tr>
-
          <?
                   if (isset($_GET['res'])){
-
                                   $cons = "SELECT * FROM sniinvestigadores WHERE vigente=1 AND nivelsni = '$res' ORDER BY area ASC";    }
                                 else{
                                           $cons = "SELECT * FROM sniinvestigadores WHERE vigente=1 ORDER BY area ASC"; }
@@ -58,7 +56,6 @@ tr:nth-child(even){ background: #FFFFFF;}
                                          include("conexion.php");
                                          mysqli_set_charset($conexion, "utf8");
                                         $consulta = mysqli_query($conexion, $cons) or die( "Error en query: $sql, el error  es: " . mysqli_error($cnx) );
-
           while($registros = mysqli_fetch_array($consulta))
                                          {
                                                 $grado  = $registros['grado'];
